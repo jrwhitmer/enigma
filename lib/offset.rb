c20d888f8754545513c274526f25454fe1dad706
Jacq Whitmer <jacqwhitmer@gmail.com>
2021-04-23 13:07:02 -0400

    Create attribute methods for offset class

--- lib/offset.rb
+++ lib/offset.rb
@@ -9,5 +9,19 @@ class Offset
     @offset = generate_offset(date)
   end
 
+  def offset_a
+    @offset.to_s[0].to_i
+  end
 
+  def offset_b
+    @offset.to_s[1].to_i
+  end
+
+  def offset_c
+    @offset.to_s[2].to_i
+  end
+
+  def offset_d
+    @offset.to_s[3].to_i
+  end
 end
