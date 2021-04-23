2ab0846c971a1bdec0560c5ae872072b4c9a2695
Jacq Whitmer <jacqwhitmer@gmail.com>
2021-04-23 10:47:06 -0400

    Create test for attributes of key class

--- spec/key_spec.rb
+++ spec/key_spec.rb
@@ -11,6 +11,15 @@ RSpec.describe Key do
       expect(key).to be_instance_of(Key)
     end
 
+    it 'has attributes' do
+      key = Key.new(04726)
+
+      expect(key.a_key).to eq(04)
+      expect(key.b_key).to eq(47)
+      expect(key.c_key).to eq(72)
+      expect(key.d_key).to eq(26)
+    end
+
   end
 
 
