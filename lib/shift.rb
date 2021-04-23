ebfb239fef8ae3bde79738e1290586783122db29
Jacq Whitmer <jacqwhitmer@gmail.com>
2021-04-23 13:34:55 -0400

    Create methods for shifts a through d attributes of shift class

--- lib/shift.rb
+++ lib/shift.rb
@@ -3,6 +3,21 @@ class Shift
   def initialize(key, offset)
     @key = key
     @offset = offset
-  end 
+  end
 
+  def a_shift
+    @key.a_key + @offset.offset_a
+  end
+
+  def b_shift
+    @key.b_key + @offset.offset_b
+  end
+
+  def c_shift
+    @key.c_key + @offset.offset_c
+  end
+
+  def d_shift
+    @key.d_key + @offset.offset_d  
+  end
 end
--- spec/shift_spec.rb
+++ spec/shift_spec.rb
@@ -13,6 +13,18 @@ RSpec.describe Shift do
 
       expect(shift).to be_instance_of(Shift)
     end
+
+    it 'has attributes' do
+      allow_any_instance_of(Key).to receive(:rand).and_return(4837)
+      key = Key.new
+      offset = Offset.new("05-16-1998")
+      shift = Shift.new(key, offset)
+
+      expect(shift.a_shift).to eq(6)
+      expect(shift.b_shift).to eq(48)
+      expect(shift.c_shift).to eq(83)
+      expect(shift.d_shift).to eq(41)
+    end
   end
 
 end
