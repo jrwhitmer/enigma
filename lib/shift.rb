084b551453ac8b03dd2546d9c9d2efc83aec66c2
Jacq Whitmer <jacqwhitmer@gmail.com>
2021-04-23 14:12:02 -0400

    Create helper methods and tests for shift_by_a method

--- lib/shift.rb
+++ lib/shift.rb
@@ -18,6 +18,20 @@ class Shift
   end
 
   def d_shift
-    @key.d_key + @offset.offset_d  
+    @key.d_key + @offset.offset_d
+  end
+
+  def character_set
+    character_set = {"a" => 1, "b" => 2, "c" => 3, "d" => 4, "e" => 5, "f" => 6, "g" => 7, "h" => 8, "i" => 9, "j" => 10, "k" => 11, "l" => 12, "m" => 13, "n" => 14, "o" => 15, "p" => 16, "q" => 17, "r" => 18, "s" => 19, "t" => 20, "u" => 21, "v" => 22, "w" => 23, "x" => 24, "y" => 25, "z" => 26, " " => 27}
+  end
+
+  def translate_text_to_values(text)
+    text.chars.map do |character|
+      character_set[character.downcase]
+     end
+  end
+
+  def shift_by_a(text)
+    translate_text_to_values(text)
   end
 end
--- spec/shift_spec.rb
+++ spec/shift_spec.rb
@@ -24,18 +24,29 @@ RSpec.describe Shift do
       expect(shift.b_shift).to eq(48)
       expect(shift.c_shift).to eq(83)
       expect(shift.d_shift).to eq(41)
+      expect(shift.character_set).to eq( {"a" => 1, "b" => 2, "c" => 3, "d" => 4, "e" => 5, "f" => 6, "g" => 7, "h" => 8, "i" => 9, "j" => 10, "k" => 11, "l" => 12, "m" => 13, "n" => 14, "o" => 15, "p" => 16, "q" => 17, "r" => 18, "s" => 19, "t" => 20, "u" => 21, "v" => 22, "w" => 23, "x" => 24, "y" => 25, "z" => 26, " " => 27})
     end
   end
 
   context 'methods' do
-    allow_any_instance_of(Key).to receive(:rand).and_return(4837)
-    key = Key.new
-    offset = Offset.new("05-16-1998")
-    shift = Shift.new(key, offset)
-    text = "This is test text"
+    it 'can translate text into an array of values associated with our character set' do
+      allow_any_instance_of(Key).to receive(:rand).and_return(4837)
+      key = Key.new
+      offset = Offset.new("05-16-1998")
+      shift = Shift.new(key, offset)
+      text = "This is test text"
+
+      expect(shift.translate_text_to_values(text)).to eq([20, 8, 9, 19, 27, 9, 19, 27, 20, 5, 19, 20, 27, 20, 5, 24, 20])
+    end
 
     it 'can shift text by a_shift amount' do
-      expect(shift.shift_by_a(text).to eq()
+      allow_any_instance_of(Key).to receive(:rand).and_return(4837)
+      key = Key.new
+      offset = Offset.new("05-16-1998")
+      shift = Shift.new(key, offset)
+      text = "This is test text"
+
+      expect(shift.shift_by_a(text)).to eq("")
     end
   end
 
