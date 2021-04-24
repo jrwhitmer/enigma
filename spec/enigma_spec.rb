f0a83c0b8c0c7bae694d293715d957c243c8a189
Jacq Whitmer <jacqwhitmer@gmail.com>
2021-04-24 18:11:45 -0400

    Create test for enigma instantiation

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -1,1 +1,12 @@
 require './spec/spec_helper'
+require './lib/enigma'
+
+RSpec.describe Enigma do
+  context 'instantiation' do
+    it 'exists' do
+      enigma = Enigma.new
+
+      expect(enigma).to be_instance_of(Enigma)
+    end
+  end
+end
