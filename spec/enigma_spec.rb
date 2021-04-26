58953e2b6919c03741d2b99dfee436c2ed14c72f
Jacq Whitmer <jacqwhitmer@gmail.com>
2021-04-26 09:54:49 -0400

    Add default value for key in encrypt method to be a randomly generated key

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -31,5 +31,13 @@ RSpec.describe Enigma do
       expect(enigma.encrypt(message, key)).to eq({encryption: "jjgshbokjjks!", key: "04837", date: "260421"})
     end
 
+    it 'can encrypt a message with a default random key' do
+      message = "Blah di bleh!"
+      allow_any_instance_of(Key).to receive(:rand).and_return(4867)
+      enigma = Enigma.new
+
+      expect(enigma.encrypt(message)).to eq({encryption: "jjjvhbrnjjnv!", key: "04867", date: "260421"})
+    end
+
   end
 end
