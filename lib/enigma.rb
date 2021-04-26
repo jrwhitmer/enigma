2e51f3c3edd9c3091088a80a9f3e60a7a63ccfdf
Jacq Whitmer <jacqwhitmer@gmail.com>
2021-04-26 09:24:04 -0400

    Create helper method for encrypt, set_shift

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -5,23 +5,24 @@ require 'date'
 
 class Enigma
 
-  def initialize
+  def set_shift(message, key, date)
+    offset = Offset.new(date)
+    @shift = Shift.new(key, offset)
   end
 
   def encrypt(message, key, date)
-    offset = Offset.new(date)
-    shift = Shift.new(key, offset)
+    set_shift(message, key, date)
     encryption_message = []
     counter = 0
     message.chars.cycle(1) do |character|
       if counter == 0 || counter.remainder(4) == 0
-        encryption_message << shift.translate_a_values_to_text(character)
+        encryption_message << @shift.translate_a_values_to_text(character)
       elsif counter.remainder(4) == 1
-        encryption_message << shift.translate_b_values_to_text(character)
+        encryption_message << @shift.translate_b_values_to_text(character)
       elsif counter.remainder(4) == 2
-        encryption_message << shift.translate_c_values_to_text(character)
+        encryption_message << @shift.translate_c_values_to_text(character)
       elsif counter.remainder(4) == 3
-        encryption_message << shift.translate_d_values_to_text(character)
+        encryption_message << @shift.translate_d_values_to_text(character)
       end
       counter += 1
     end
